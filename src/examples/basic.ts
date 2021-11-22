5f98810e5bf5f3bb25adcf4c70fbc720b0ea1331
Assembly Guy <alexspinu.dev@gmail.com>
2021-11-22 17:01:47 +0200

    dsadsa

--- src/examples/basic.ts
+++
@@ -1,6 +0,0 @@
-import {isTar} from  '../index';
-
-const validFileName     = isTar("fds.tar.gz");                          // true
-const invalidFileName   = isTar("fds.tar.gz", true);                    // false
-const validExtension    = isTar(".tar.gz");                             // true
-
